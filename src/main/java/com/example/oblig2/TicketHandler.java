1cf7e47688d1c7aa8217c0544a9eb80336035b16
Eirik Helgerud Kristoffersen <eirik.h.kristoffersen@gmail.com>
2024-03-14 17:05:24 +0100

    final

---
+++ src/main/java/com/example/oblig2/TicketHandler.java
@@ -0,0 +1,36 @@
+package com.example.oblig2;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+
+@RestController
+public class TicketHandler {
+
+    private ArrayList<Ticket> ticketList = new ArrayList<>();
+    @PostMapping(path = "/addTicket",
+        consumes = MediaType.APPLICATION_JSON_VALUE,
+        produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public void addTicket(@RequestBody Ticket ticket) {
+        System.out.println(ticket);
+        ticketList.add(ticket);
+
+    }
+
+    @GetMapping("/getList")
+    public ArrayList<Ticket> hey() {
+        System.out.println("getReq");
+        return ticketList;
+    }
+
+    @PostMapping("/delete")
+    public void deleteList() {
+        ticketList.clear();
+    }
+
+
+}
